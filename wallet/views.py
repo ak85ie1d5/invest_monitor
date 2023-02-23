19decec52315407b3b641760910ffac59ba73146
Jeremy <jspaeth@prevor.com>
2023-02-23 12:40:01 +0100

    Adding first front view.

--- wallet/views.py
+++ wallet/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Product
+from .models import Underlying
 
 def index(request):
     product_list = Product.objects.order_by('name')
+    underlying_list = Underlying.objects.order_by('name')
     context = {
         'product_list': product_list,
+        'underlying_list': underlying_list
     }
     return render(request, 'wallet/index.html', context)
