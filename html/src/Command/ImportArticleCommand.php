0a3140e450ff86f19be30bc0bf24e09e49c6073f
Jeremy SPAETH <jeremy@businessprofilers.fr>
2026-08-09 20:18:08 +0200

    Fetch all article from BourseDirect timeline

--- html/src/Command/ImportArticleCommand.php
+++ html/src/Command/ImportArticleCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
@@ -21,6 +22,8 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 )]
 class ImportArticleCommand extends Command
 {
+    private const URL = 'https://www.boursedirect.fr/fr/actualites/categorie/turbos';
+
     public function __construct(private HttpClientInterface $client)
     {
         parent::__construct();
@@ -31,7 +34,7 @@ class ImportArticleCommand extends Command
     }
 
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         //$io = new SymfonyStyle($input, $output);
         //$io->success('You have a new command! Now make it your own! Pass --help to see your options.');
@@ -39,7 +42,14 @@ class ImportArticleCommand extends Command
 
         $response = $this->client->request(
             'GET',
-            'https://www.boursedirect.fr/fr/actualites/categorie/turbos'
+            self::URL,
+            [
+                'headers' => [
+                    'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+                    'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                    'Accept-Language' => 'fr-FR,fr;q=0.9',
+                ],
+            ]
         );
 
         if ($response->getStatusCode() !== 200) {
@@ -47,6 +57,31 @@ class ImportArticleCommand extends Command
 
             return Command::FAILURE;
         }
+
+        $crawler = new Crawler($response->getContent(), self::URL);
+
+        $articles = $crawler->filter('div.timeline-item')->each(function (Crawler $node): array {
+            $link = $node->filter('div.timeline-body a');
+            $date = $node->filter('div.timeline-date');
+
+            return [
+                'title' => trim($node->filter('h2.timeline-title')->text('')),
+                'url' => $link->count() ? $link->link()->getUri() : null,
+                'hour' => trim($node->filter('div.timeline-date-left')->text('')),
+                'date' => trim(implode(' ', [
+                    trim($date->filter('span.publishDay')->text('')),
+                    trim($date->filter('strong')->text('')),
+                    trim($date->filter('span.text-muted')->text('')),
+                ])),
+            ];
+        });
+
+        foreach ($articles as $article) {
+            $output->writeln(sprintf('[%s %s] %s', $article['date'], $article['hour'], $article['title']));
+            $output->writeln('  '.$article['url']);
+        }
+
+        $output->writeln(sprintf('<info>%d articles trouvés.</info>', \count($articles)));
 
         return Command::SUCCESS;
 
