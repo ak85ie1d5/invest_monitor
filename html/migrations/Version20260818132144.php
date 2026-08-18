01041d520060dd596e637987d8b655e9b47635c3
Jeremy SPAETH <jeremy@businessprofilers.fr>
2026-08-18 15:22:52 +0200

    Add entity to save Products

---
+++ html/migrations/Version20260818132144.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260818132144 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(50) NOT NULL, product_type VARCHAR(255) NOT NULL, product_direction VARCHAR(255) NOT NULL, isin VARCHAR(12) NOT NULL, stop_loss_level DOUBLE PRECISION DEFAULT NULL, stop_loss_currency VARCHAR(255) NOT NULL, end_date DATE DEFAULT NULL, leverage DOUBLE PRECISION NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE product');
+    }
+}
---
+++ html/src/Entity/Product.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\Currency;
+use App\Enum\ProductDirection;
+use App\Enum\ProductType;
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column(enumType: ProductType::class)]
+    private ?ProductType $productType = null;
+
+    #[ORM\Column(enumType: ProductDirection::class)]
+    private ?ProductDirection $productDirection = null;
+
+    #[ORM\Column(length: 12)]
+    private ?string $Isin = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $StopLossLevel = null;
+
+    #[ORM\Column(enumType: Currency::class)]
+    private ?Currency $stopLossCurrency = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTime $endDate = null;
+
+    #[ORM\Column]
+    private ?float $leverage = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getProductType(): ?ProductType
+    {
+        return $this->productType;
+    }
+
+    public function setProductType(ProductType $productType): static
+    {
+        $this->productType = $productType;
+
+        return $this;
+    }
+
+    public function getProductDirection(): ?ProductDirection
+    {
+        return $this->productDirection;
+    }
+
+    public function setProductDirection(ProductDirection $productDirection): static
+    {
+        $this->productDirection = $productDirection;
+
+        return $this;
+    }
+
+    public function getIsin(): ?string
+    {
+        return $this->Isin;
+    }
+
+    public function setIsin(string $Isin): static
+    {
+        $this->Isin = $Isin;
+
+        return $this;
+    }
+
+    public function getStopLossLevel(): ?float
+    {
+        return $this->StopLossLevel;
+    }
+
+    public function setStopLossLevel(?float $StopLossLevel): static
+    {
+        $this->StopLossLevel = $StopLossLevel;
+
+        return $this;
+    }
+
+    public function getStopLossCurrency(): ?Currency
+    {
+        return $this->stopLossCurrency;
+    }
+
+    public function setStopLossCurrency(Currency $stopLossCurrency): static
+    {
+        $this->stopLossCurrency = $stopLossCurrency;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTime
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(?\DateTime $endDate): static
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    public function getLeverage(): ?float
+    {
+        return $this->leverage;
+    }
+
+    public function setLeverage(float $leverage): static
+    {
+        $this->leverage = $leverage;
+
+        return $this;
+    }
+}
