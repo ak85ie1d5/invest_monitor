2a2ac64437e077e61bd6c99d9b78c557bc2b39df
Jeremy SPAETH <jeremy@businessprofilers.fr>
2026-08-19 17:50:24 +0200

    Adding controller to list all products

---
+++ html/src/Controller/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'app_product')]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $products = $entityManager->getRepository(Product::class)->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'title' => 'Product',
+            'products' => $products,
+        ]);
+    }
+}
