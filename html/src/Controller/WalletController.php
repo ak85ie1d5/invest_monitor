c314b81cff26ccb5a755f4f7077fa4a57cbdfc21
Jeremy SPAETH <jeremy@businessprofilers.fr>
2026-08-19 10:49:18 +0200

    Adding form to save products in the wallet

---
+++ html/src/Controller/WalletController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Wallet;
+use App\Form\ProductType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class WalletController extends AbstractController
+{
+    #[Route('/wallet', name: 'app_wallet')]
+    public function index(): Response
+    {
+        return $this->render('wallet/index.html.twig', [
+            'controller_name' => 'WalletController',
+        ]);
+    }
+
+    #[Route('/wallet/add-product', name: 'app_wallet_add_product', methods: ['GET', 'POST'])]
+    public function addProduct(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $wallet = new Wallet();
+        $form = $this->createForm(ProductType::class, $wallet);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($wallet);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Produit ajouté au portefeuille.');
+
+            return $this->redirectToRoute('app_wallet');
+        }
+
+        return $this->render('wallet/add_product.html.twig', [
+            'title' => "Ajouter un produit",
+            'form' => $form->createView()
+        ]);
+    }
+}
